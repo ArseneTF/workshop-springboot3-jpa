c4cdff16be0cdf48f73d4d3032b4f3e18485e41e
Angelo Souza <angelogus22@gmail.com>
2023-06-27 10:22:00 -0300

    Project created

---
+++ src/main/java/com/student/mproject/MprojectApplication.java
@@ -0,0 +1,13 @@
+package com.student.mproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/student/mproject/MprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.student.mproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
